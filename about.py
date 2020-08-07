0644c97b20e83db1f895e751d818cfc696c817e5
CastaliaIkz <68614343+Kagurazaga@users.noreply.github.com>
2020-08-07 20:25:29 +0000

    Update about.py

--- about.py
+++ about.py
@@ -1,10 +1,16 @@
+import os,sys,time
 def jalan (kata):
  for e in kata:
   sys.stdout.write(e)
   sys.stdout.flush()
   time.sleep(0.1)
-jalan('\033[1;91mBuat lu para decoder script, gua peringatkan\033[0m\n'.center(44))
-jalan('\033[1;91mKalo lu berani nyolong script gua tanpa seijin gua\033[0m\n'.center(44))
-jalan('\033[1;91minget!. gua bakal datang ke rumah lu, gua tusuk lobang p****t lu\033[0m\n'.center(44))
-jalan('\033[1;91minget itu anjing, bikin SC ga gampang ya ppq\033[0m\n'.center(44))
-jalan('\033[1;32mBtw, Gue ganteng tq.!\033[0m\n'.center(44))
+jalan('\033[1;91mScript masih dalam proses pengembangan, belum selesai\033[0m\n'.center(44))
+jalan('\033[1;91mMungkin akan gua update nanti, tapi gatau kapan\033[0m\n'.center(44))
+jalan('\033[1;91mBikin sc python ngoding, ga gampang dek, sabar oke\033[0m\n'.center(44))
+jalan('\033[1;32mWhatsApp : 089614402699\033[0m\n'.center(44))
+jalan('\033[1;32mFacebook : Bagaskurniawan Ex\033[0m\n'.center(44))
+jalan('\033[1;32mInstagram : kz_206\033[0m\n'.center(44))
+jalan('\033[1;32mPastebin : https://github.com/kagurazaga\033[0m\n'.center(44))
+jalan('\033[1;32mWebsite : xgt-cyber.my.id\033[0m\n'.center(44))
+jalan('\033[1;36mThanks God\nSpecial Thanks to NsaaLvd\n\Greetz All Member XamppGangstaTeam\033[0m\n'.center(44))
+jalan('\033[0;36mSee you, Goodbye:)x\033[0m\n'.center(44))
