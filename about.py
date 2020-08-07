63dbea128bca3f6a9f830e285baa44aa0a1e68b1
CastaliaIkz <68614343+Kagurazaga@users.noreply.github.com>
2020-08-07 20:13:23 +0000

    Create about.py

---
+++ about.py
@@ -0,0 +1,10 @@
+def jalan (kata):
+ for e in kata:
+  sys.stdout.write(e)
+  sys.stdout.flush()
+  time.sleep(0.1)
+jalan('\033[1;91mBuat lu para decoder script, gua peringatkan\033[0m\n'.center(44))
+jalan('\033[1;91mKalo lu berani nyolong script gua tanpa seijin gua\033[0m\n'.center(44))
+jalan('\033[1;91minget!. gua bakal datang ke rumah lu, gua tusuk lobang p****t lu\033[0m\n'.center(44))
+jalan('\033[1;91minget itu anjing, bikin SC ga gampang ya ppq\033[0m\n'.center(44))
+jalan('\033[1;32mBtw, Gue ganteng tq.!\033[0m\n'.center(44))
